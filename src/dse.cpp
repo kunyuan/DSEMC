e37062c1643061ea939f896be5b9779cb1508f2d
Kun Chen <chenkun@mail.ustc.edu.cn>
2019-08-06 19:15:38 -0400

    wip

--- src/dse.cpp
+++ src/dse.cpp
@@ -10,36 +10,58 @@
 using namespace dse;
 using namespace std;
 
-dse::ver4 *verDiag::Ver0(array<int, 4> LegK, array<int, 2> InLegT,
-                         bool IsBare) {
+#define SETK(Ver4, LegK)                                                       \
+  Ver4.LegK[INL] = LegK[INL];                                                  \
+  Ver4.LegK[INR] = LegK[INR];                                                  \
+  Ver4.LegK[OUTL] = LegK[OUTL];                                                \
+  Ver4.LegK[OUTR] = LegK[OUTR];
 
-  //   return VerQTheta.Interaction(InL, InR, DirTran, 0, Var.CurrScale) -
-  //   VerQTheta.Interaction(InL, InR, ExTran, 0, Var.CurrScale);
-  //   double Tau = Var.Tau[TauIndex] - Var.Tau[TauIndex + 1];
-  //   int &Index = _DiagIndex[Level];
-  //   ////////////// bare interaction ///////////
-  //   double DiWeight = VerQTheta.Interaction(InL, InR, DirTran, Tau, 0);
-  //   double ExWeight =
-  //       VerQTheta.Interaction(InL, InR, InR + DirTran - InL, Tau, 0);
-  //   SETTAU(_GlobalOrder, Level, Index, TauIndex, TauIndex, TauIndex,
-  //   TauIndex); _Weight[Level][Index][0] = DiWeight - ExWeight;
-  //   // _Weight[Level][DiagIndex][0] = DiWeight;
-  //   Index += 1;
+#define SETINT(Ver4, InT)                                                      \
+  Ver4.InT[INL] = InT[INL];                                                    \
+  Ver4.InT[INR] = InT[INR];
 
-  //   if (Type != -2) {
-  //     //////////// dressed interaction ///////////
-  //     DiWeight = VerQTheta.Interaction(InL, InR, DirTran, Tau, 1);
-  //     SETTAU(_GlobalOrder, Level, Index, TauIndex, TauIndex, TauIndex + 1,
-  //            TauIndex + 1);
-  //     _Weight[Level][Index][0] = DiWeight;
-  //     Index += 1;
+#define SETOUTT(Ver4, DiagNum, OutL, OutR)                                     \
+  Ver4.OutT[DiagNum][OUTL] = OutL;                                             \
+  Ver4.OutT[DiagNum][OUTR] = OutR;
 
-  //     ExWeight = VerQTheta.Interaction(InL, InR, InR + DirTran - InL, Tau,
-  //     1); SETTAU(_GlobalOrder, Level, Index, TauIndex, TauIndex + 1, TauIndex
-  //     + 1,
-  //            TauIndex);
-  //     _Weight[Level][Index][0] = -ExWeight;
-  //     Index += 1;
-  //   }
-  return;
+ver4 *verDiag::Ver0(array<int, 4> LegK, array<int, 2> InT, bool IsBare) {
+  ver4 &Ver4 = Pool.Ver4[Pool.Ver4Index];
+  //   struct ver4 {
+  //     int Level;
+  //     int DiagNum;
+  //     int Type;
+  //     bool IsProjection;
+  //     ver4 *SubVer[2];
+  //     momentum *LegK[4];
+  //     int LegT[4];
+  //     green *Internal[MaxOrder * 2];
+  //     double Weight[MaxOrder * MaxOrder * 4];
+  //   };
+  Ver4.LoopNum = 0;
+  ////////////// bare interaction ///////////
+  Ver4.DiagNum = 1;
+  Ver4.Type[Ver4.DiagNum] = BARE;
+  SETK(Ver4, LegK);
+  SETINT(Ver4, InT);
+  SETOUTT(Ver4, Ver4.DiagNum, InT[INL], InT[INR]);
+
+  if (IsBare == false) {
+    //////////// dressed interaction ///////////
+    Ver4.DiagNum++;
+    SETOUTT(Ver4, Ver4.DiagNum, InT[INL], InT[INR]);
+    //     DiWeight = VerQTheta.Interaction(InL, InR, DirTran, Tau, 1);
+    //     SETTAU(_GlobalOrder, Level, Index, TauIndex, TauIndex, TauIndex + 1,
+    //            TauIndex + 1);
+    //     _Weight[Level][Index][0] = DiWeight;
+    //     Index += 1;
+
+    //     ExWeight = VerQTheta.Interaction(InL, InR, InR + DirTran - InL, Tau,
+    //     1); SETTAU(_GlobalOrder, Level, Index, TauIndex, TauIndex + 1,
+    //     TauIndex
+    //     + 1,
+    //            TauIndex);
+    //     _Weight[Level][Index][0] = -ExWeight;
+    //     Index += 1;
+  }
+  return &Ver4;
 }
--- src/dse.h
+++ src/dse.h
@@ -3,6 +3,7 @@
 
 #include "global.h"
 #include "utility/utility.h"
+#include "vertex.h"
 #include <array>
 #include <string>
 #include <vector>
@@ -14,19 +15,28 @@ using namespace std;
 
 struct green {
   momentum *Mom;
+  int Type;
   int Tau[2];
   double Weight;
 };
 
+const int MAXDIAG = MaxOrder * MaxOrder * 4;
+enum vertype { BARE, DYNAMIC, NORMAL, PROJECTED, RENORMALIZED };
+
 struct ver4 {
   int Level;
   int DiagNum;
+  vertype Type[MAXDIAG]; //-2: bare coupling, -1: dynamic coupling, 0: normal
+                         // vertex, 1:
+                         // projected vertex, 2: renormalized vertex
+  int LoopNum;
   bool IsProjection;
   ver4 *SubVer[2];
-  momentum *LegK[4];
-  int LegT[4];
+  int LegK[4];
+  int InT[2];
+  int OutT[MAXDIAG][2];
   green *Internal[MaxOrder * 2];
-  double Weight[MaxOrder * MaxOrder * 4];
+  // double Weight[MaxOrder * MaxOrder * 4];
 };
 
 struct pool {
@@ -42,10 +52,18 @@ typedef array<bool, 3> channel;
 
 class verDiag {
 public:
+  // verDiag(array<momentum, MaxLoopNum> &loopmom, array<double, MaxTauNum>
+  // &tau)
+  //     : LoopMom(loopmom), Tau(tau){};
   void Build(int LoopNum);
   pool Pool;
 
 private:
+  // array<momentum, MaxLoopNum> &LoopMom; // all momentum loop variables
+  // array<double, MaxTauNum> &Tau;        // all tau variables
+
+  // verQTheta VerWeight;
+
   ver4 *Ver4AtOrder(
       array<int, 4> LegK, array<int, 2> InLegT,
       int Type // -1: DSE diagrams, 0: normal digrams, 1: renormalzied diagrams
